c69416d754ee4ba4722f68043141a5b6b88b333f
lilianni26 <lilibarbosanayara@gmail.com>
2024-11-08 14:51:54 -0400

    Add files via upload

---
+++ _font style=_vertical-align_ inherit;___font class=_custom-cursor-default-hover_ style=_vertical-align_ inherit;__principal.py
@@ -0,0 +1,25 @@
+#Autor: Lilianni Nayara Barbosa Mendonça
+#Data: 08/11/2024
+#Teoria do IMC 
+
+def calcula_imc(peso, altura):
+      print("Parâmetro peso:", 52)
+      print("Parâmetro altura:", 1.50)
+      imc = peso/altura**2
+      print("IMC:", imc)
+      
+#Início do programa
+print("Inicio do cálculo")
+calcula_imc(52,1.50)
+print("Término do programa")
+
+def calcula_imc(peso, altura):
+     print ("Parâmetro peso:", 45)
+     print("Parâmetro altura:", 1.55)
+     imc = peso/altura**2
+     print("IMC:", imc)
+     
+#Início do programa
+print("Início do cálculo")
+calcula_imc(45, 1.55)
+print("Término do programa")
